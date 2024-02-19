d673420c5757ffec1e23024c9ade4ada70781207
Mohnish2004 <mohnishgopi@gmail.com>
2024-02-19 15:05:59 -0800

    Update client.cs

--- client.cs
+++ client.cs
@@ -1,6 +1,4 @@
-
 using UnityEngine;
-using System.Collections;
 using System;
 using System.Text;
 using System.Net;
@@ -9,23 +7,35 @@ using System.Threading;
 
 public class UdpSocket : MonoBehaviour
 {
-    [HideInInspector] public bool isTxStarted = false;
-    [SerializeField] string IP = "127.0.0.1"; // local host
-    [SerializeField] int rxPort = 8000; // port to receive data from Python on
-    [SerializeField] int txPort = 8001; // port to send data to Python on
-    int i = 0; // DELETE THIS: Added to show sending data from Unity to Python via UDP
-    // Create necessary UdpClient objects
+    [SerializeField] string IP = "127.0.0.1"; // Local host
+    [SerializeField] int rxPort = 8000; // Port to receive data from Python on
+    [SerializeField] int txPort = 8001; // Port to send data to Python on
+
     UdpClient client;
     IPEndPoint remoteEndPoint;
-    Thread receiveThread; // Receiving Thread
-    IEnumerator SendDataCoroutine() // DELETE THIS: Added to show sending data from Unity to Python via UDP
+    int i = 0; // Counter for the numbers to send
+
+    void Start()
     {
-        while (true)
-        {
-            SendData("Sent from Unity: " + i.ToString());
-            i++;
-            yield return new WaitForSeconds(1f);
-        }
+        // Create remote endpoint (to Python) 
+        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), txPort);
+
+        // Create local client
+        client = new UdpClient(rxPort);
+
+        // Start a thread for reception of incoming messages
+        Thread receiveThread = new Thread(new ThreadStart(ReceiveData));
+        receiveThread.IsBackground = true;
+        receiveThread.Start();
+
+        Debug.Log("UDP Comms Initialized");
+    }
+
+    void Update()
+    {
+        // Send data continuously
+        SendData("Sent from Unity: " + i.ToString());
+        i++;
     }
 
     public void SendData(string message) // Use to send data to Python
@@ -37,39 +47,12 @@ public class UdpSocket : MonoBehaviour
         }
         catch (Exception err)
         {
-            print(err.ToString());
+            Debug.LogError(err.ToString());
         }
     }
 
-    void Awake()
+    void ReceiveData()
     {
-        // Create remote endpoint (to Python) 
-        remoteEndPoint = new IPEndPoint(IPAddress.Parse(IP), txPort);
-
-        // Create local client
-        client = new UdpClient(rxPort);
-
-        // local endpoint define (where messages are received)
-        // Create a new thread for reception of incoming messages
-        Thread receiveThread = new Thread(new ThreadStart(ReceiveData));
-        receiveThread.IsBackground = true;
-        receiveThread.Start();
-
-        // Initialize (seen in comments window)
-        print("UDP Comms Initialised");
-
-        // Start sending numbers immediately
-        StartCoroutine(SendNumbers());
+        // Add your code for receiving data from Python here
     }
-
-    IEnumerator SendNumbers()
-    {
-        while (true)
-        {
-            SendData("Sent from Unity: " + i.ToString());
-            i++;
-            yield return null; // Allow the next frame to proceed without delay
-        }
-    }
-
 }
